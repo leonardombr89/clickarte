9cd38706900db51bc5d2ce3ffd4edf261a2f0834
wellington fernandes <wellingtonfds@gmail.com>
2024-08-13 10:47:43 -0300

    add const

--- src/main/java/com/clickarte/crm/controllers/category/CategoryController.java
+++ src/main/java/com/clickarte/crm/controllers/category/CategoryController.java
@@ -5,10 +5,6 @@ package com.clickarte.crm.controllers.category;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
-
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.util.UriComponentsBuilder;
 import com.clickarte.crm.dtos.category.CreateCategoryDto;
